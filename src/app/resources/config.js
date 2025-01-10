07e65a44d7b233d03b158b3f7617545c3b013c1d
Peter Riemersma <35268498+GroteGehaktBal@users.noreply.github.com>
2025-01-10 21:05:00 +0100

    testfix

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -6,7 +6,7 @@ const i18n = true;
 // Manage localized content in the messages folder
 const i18nOptions = {
     locales: ['en','nl'],            // A list of all locales that are supported, e.g. ['en','id']
-    defaultLocale: 'nl'         // Locale used by default and as a fallback
+    defaultLocale: 'en'         // Locale used by default and as a fallback
 }
 
 const routes = {
