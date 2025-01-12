bfb162e6d89a9b6f2303dda781109de02f7c0ece
Peter Riemersma <35268498+GroteGehaktBal@users.noreply.github.com>
2025-01-12 02:15:03 +0100

    fix1

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,24 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+    webpack: (config, { isServer }) => {
+        // Exclude unnecessary files
+        config.plugins.push(
+            new config.webpack.IgnorePlugin({
+                resourceRegExp: /\.(spec|test)\.(js|ts)x?$/,
+                contextRegExp: /pages/,
+            })
+        );
+
+        // Optionally, you can also use the `exclude` option
+        // config.module.rules.push({
+        //     test: /\.(js|ts)x?$/,
+        //     exclude: /\.(spec|test)\.(js|ts)x?$/,
+        //     use: 'babel-loader',
+        // });
+
+        return config;
+    },
 };
 
 export default withNextIntl(withMDX(nextConfig));
