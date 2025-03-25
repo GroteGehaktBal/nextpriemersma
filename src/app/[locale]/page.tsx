dcc87d3fe57c7df79c985ea97343a8c7a696cb44
GroteGehaktBal <peterriemersma15@gmail.com>
2025-03-25 17:19:40 +0100

    revert to original page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
-import { Heading, Flex, Text, Button,  Avatar, RevealFx } from '@/once-ui/components';
+import { Heading, Flex, Text, Button, Avatar, RevealFx } from '@/once-ui/components';
 import { Projects } from '@/components/work/Projects';
 
-import { baseURL, routes, renderContent } from '@/app/resources'; 
+import { baseURL, routes, renderContent } from '@/app/resources';
 import { Mailchimp } from '@/components';
 import { Posts } from '@/components/blog/Posts';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { useTranslations } from 'next-intl';
 
-export async function generateMetadata(props: { params: Promise<{ locale: string }>}) {
-    const params = await props.params;
-
-    const {
-        locale
-    } = params;
-
+export async function generateMetadata(
+	{ params: { locale } }: { params: { locale: string } }
+) {
 	const t = await getTranslations();
-    const { home } = renderContent(t);
+	const { home } = renderContent(t);
 	const title = home.title;
 	const description = home.description;
 	const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
@@ -46,16 +42,12 @@ export async function generateMetadata(props: { params: Promise<{ locale: string
 	};
 }
 
-export default function Home(props: { params: Promise<{ locale: string }>}) {
-    const params = use(props.params);
-
-    const {
-        locale
-    } = params;
-    
-    unstable_setRequestLocale(locale);
-    const t = useTranslations();
-    const { home, about, person, newsletter } = renderContent(t);
+export default function Home(
+	{ params: { locale } }: { params: { locale: string } }
+) {
+	unstable_setRequestLocale(locale);
+	const t = useTranslations();
+	const { home, about, person, newsletter } = renderContent(t);
 	return (
 		<Flex
 			maxWidth="m" fillWidth gap="xl"
@@ -86,60 +78,57 @@ export default function Home(props: { params: Promise<{ locale: string }>}) {
 				fillWidth
 				direction="column"
 				paddingY="l" gap="m">
-					<Flex
-						direction="column"
-						fillWidth maxWidth="s" gap="m">
-						<RevealFx translateY="4">
-							<Heading
-								wrap="balance"
-								variant="display-strong-l">
-								{home.headline}
-							</Heading>
-						</RevealFx>
-						<RevealFx
-							translateY="8" delay={0.2} fillWidth justifyContent="flex-start" paddingBottom="m">
-							<Text
-								wrap="balance"
-								onBackground="neutral-weak"
-								variant="heading-default-xl">
-								{home.subline}
-							</Text>
-						</RevealFx>
-						<RevealFx translateY="12" delay={0.4}>
-							<Flex fillWidth>
-							<Button
-									id="about"
-								data-border="rounded"
-								href={`/${locale}/about`}
-								variant="tertiary"
-								suffixIcon="chevronRight"
-								size="m">
-								<Flex
-									gap="8"
-									alignItems="center">
-									{about.avatar.display && (
-										<Avatar
-											style={{marginLeft: '-0.75rem', marginRight: '0.25rem'}}
-											src={person.avatar}
-											size="m"/>
-										)}
-										{t("about.title")}
-								</Flex>
-							</Button>
-						</RevealFx>
-					</Flex>
-				
+				<Flex
+					direction="column"
+					fillWidth maxWidth="s" gap="m">
+					<RevealFx translateY="4">
+						<Heading
+							wrap="balance"
+							variant="display-strong-l">
+							{home.headline}
+						</Heading>
+					</RevealFx>
+					<RevealFx translateY="8" delay={0.2}>
+						<Text
+							wrap="balance"
+							onBackground="neutral-weak"
+							variant="body-default-l">
+							{home.subline}
+						</Text>
+					</RevealFx>
+					<RevealFx translateY="12" delay={0.4}>
+						<Button
+							data-border="rounded"
+							href={`/${locale}/about`}
+							variant="tertiary"
+							suffixIcon="chevronRight"
+							size="m">
+							<Flex
+								gap="8"
+								alignItems="center">
+								{about.avatar.display && (
+									<Avatar
+										style={{ marginLeft: '-0.75rem', marginRight: '0.25rem' }}
+										src={person.avatar}
+										size="m" />
+								)}
+								{t("about.title")}
+							</Flex>
+						</Button>
+					</RevealFx>
+				</Flex>
+
 			</Flex>
 			<RevealFx translateY="16" delay={0.6}>
-				<Projects range={[1,1]} locale={locale}/>
+				<Projects range={[1, 1]} locale={locale} />
 			</RevealFx>
 			{routes['/blog'] && (
 				<Flex fillWidth paddingX="20">
-					<Posts range={[1,2]} columns="2" locale={locale}/>
+					<Posts range={[1, 2]} columns="2" locale={locale} />
 				</Flex>
 			)}
-			<Projects range={[2]} locale={locale}/>
-			{ newsletter.display &&
+			<Projects range={[2]} locale={locale} />
+			{newsletter.display &&
 				<Mailchimp newsletter={newsletter} />
 			}
 		</Flex>
