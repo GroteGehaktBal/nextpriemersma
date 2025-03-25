13c954b3a083869c0f266a0fc42375a44c1fe9af
GroteGehaktBal <peterriemersma15@gmail.com>
2025-03-25 17:07:53 +0100

    fix togglebutton

--- src/once-ui/components/ToggleButton.tsx
+++ src/once-ui/components/ToggleButton.tsx
@@ -87,14 +87,15 @@ const ToggleButton = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonPro
             );
         }
 
+        // Updated Link usage without <a> tag
         return (
-            <Link href={href} passHref>
-                <a
-                    ref={ref as React.Ref<HTMLAnchorElement>}
-                    {...commonProps}
-                    {...props as AnchorProps}>
-                    {content}
-                </a>
+            <Link
+                href={href}
+                ref={ref as React.Ref<HTMLAnchorElement>}
+                {...commonProps}
+                {...props as AnchorProps}
+            >
+                {content}
             </Link>
         );
     }
